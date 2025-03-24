75e320c7aa7e1df4246ed6c00a04c51e135c43af
Mohtashim Syed <128553217+mohtashim-syed@users.noreply.github.com>
2025-03-24 11:01:28 -0500

    Update Logger.java

--- Logger.java
+++ Logger.java
@@ -2,37 +2,23 @@ import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class Logger {
+class Logger {
     public static void main(String[] args) {
         if (args.length != 1) {
-              System.err.println("Usage: java Logger <log_file>");
-              System.exit(1);
-          }
-  
-      String logFile = args[0];
-      DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-  
-      try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-           BufferedWriter writer = new BufferedWriter(new FileWriter(logFile, true))){
-        String line;
-        while((line = br.readLine()) != null) {
-          line = line.trim();
-          if(line.equals"QUIT")) {
-            break;
-          }
-  
-        String[] parts = line.split("\\s+", 2);
-        String actions = parts.length > 0 ? part[0] : "";
-        String message = parts.length > 1 ? parts[1] : "";
-  
-        String timestamp = LocalDateTime.now().format(dtf);
-        String logEntry = String.format("%s [%s] %s%n", timestamp, action, message);
-        writer.write(logEntry);
-        writer.flush();
+            System.out.println("ERROR Missing log file name");
+            return;
         }
-  
-      } catch (IOEXception e){
-        e.printStackTrace();
-      }
+        String logFile = args[0];
+        try (PrintWriter logWriter = new PrintWriter(new FileWriter(logFile, true));
+             Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNextLine()) {
+                String input = scanner.nextLine();
+                if ("QUIT".equals(input)) break;
+                logWriter.println(new Date() + " [LOG] " + input);
+                logWriter.flush();
+            }
+        } catch (IOException e) {
+            System.out.println("ERROR Could not write to log file");
+        }
     }
 }
